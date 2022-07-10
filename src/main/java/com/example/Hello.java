db13d252138372fb0638d0191df8b21e8dbdabba
Bmaili <2228921046@qq.com>
2022-07-10 19:45:45 +0800

    测试

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("source Hello");
+        System.out.println("fork Hello");
+    }
+    public void test2(){
+        String name = "你号";
+        System.out.println(name);
     }
 }
